784b41ef3aea5f26d3a06b907fa7904eb93286d4
tawargi <tawargi.m@gmail.com>
2020-12-13 16:05:58 +0200

    init

--- frontend/src/js/index.js
+++
@@ -1,8 +0,0 @@
-// Global app controller
-import axios from 'axios';
-
-async function getData() {
-  const res = await axios('https://jsonplaceholder.typicode.com/albums');
-  console.log(res);
-}
-getData();
