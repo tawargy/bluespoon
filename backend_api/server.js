90142153cb9c2553c8b34addb9e2ec020bf48211
tawargi <tawargi.m@gmail.com>
2020-12-14 22:41:47 +0200

    initialize

--- backend_api/server.js
+++
@@ -1,8 +0,0 @@
-const dotenv = require("dotenv");
-dotenv.config({ path: "./config.env" });
-
-const app = require("./app");
-
-app.listen(process.env.PORT, () => {
-  console.log(" server running");
-});
